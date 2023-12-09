9054deefa485e5a71b7b6a759b223e7132571b93
bambma <spqj523@naver.com>
2023-12-09 20:42:40 +0900

    login, main, map first task

--- app/src/main/java/com/example/perfectholmes_app/MainActivity.java
+++ app/src/main/java/com/example/perfectholmes_app/MainActivity.java
@@ -1,74 +1,41 @@
 package com.example.perfectholmes_app;
 
-import android.graphics.Color;
-import android.os.Bundle;
-import android.util.Log;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
 
-import com.naver.maps.geometry.LatLng;
-import com.naver.maps.map.MapFragment;
-import com.naver.maps.map.NaverMap;
-import com.naver.maps.map.OnMapReadyCallback;
-import com.naver.maps.map.overlay.CircleOverlay;
-import com.naver.maps.map.overlay.Marker;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity {
 
-    private Marker clickedMarker;
-    private CircleOverlay circleOverlay;
+    private ImageView lifestyle;
+    private ImageView map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FragmentManager fm = getSupportFragmentManager();
-        MapFragment mapFragment = (MapFragment) fm.findFragmentById(R.id.map);
-        if (mapFragment == null) {
-            mapFragment = MapFragment.newInstance();
-            fm.beginTransaction().add(R.id.map, mapFragment).commit();
-        }
-
-        mapFragment.getMapAsync(this);
-    }
-
-    @Override
-    public void onMapReady(@NonNull NaverMap naverMap) {
-        // 목포의 좌표를 사용하여 지도 초기화
-        LatLng initialPosition = new LatLng(34.7915, 126.3922);
-        naverMap.moveCamera(com.naver.maps.map.CameraUpdate.scrollTo(initialPosition));
-
-        // 지도를 클릭할 때 이벤트 처리
-        naverMap.setOnMapClickListener((point, coord) -> {
-            if (clickedMarker != null) {
-                clickedMarker.setMap(null); // 이전 마커 삭제
-                clickedMarker = null;
-                if (circleOverlay != null) {
-                    circleOverlay.setMap(null); // 이전 원 삭제
-                    circleOverlay = null;
-                }
-            } else {
-                // 클릭된 위치에 마커 추가
-                clickedMarker = new Marker();
-                clickedMarker.setPosition(new LatLng(coord.latitude, coord.longitude));
-                clickedMarker.setMap(naverMap);
-
-                // 클릭된 위치를 중심으로 반경 1km의 원 추가
-                circleOverlay = new CircleOverlay();
-                circleOverlay.setCenter(new LatLng(coord.latitude, coord.longitude));
-                circleOverlay.setRadius(1000); // 반경 1km
-                circleOverlay.setColor(Color.argb(50, 0, 0, 255)); // 반투명한 파란색
-                circleOverlay.setMap(naverMap);
-
-                // 마커를 찍은 위치의 경도와 위도 출력
-                double clickedLatitude = coord.latitude;
-                double clickedLongitude = coord.longitude;
-                String coordinates = String.format("위도: %f, 경도: %f", clickedLatitude, clickedLongitude);
-                Log.d("MapClickCoordinates", coordinates);
+        lifestyle = findViewById(R.id.lifestyle_img);
+        lifestyle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, lifestyle_test.class);
+                startActivity(intent);
             }
         });
+
+
+
+        map = findViewById(R.id.map_img);
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Map.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
