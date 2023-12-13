0b0f00e3d5a05c720a75830fca7106ef8ce845f4
seasonkhadka <seasonkhadka344@gmail.com>
2023-12-14 01:58:02 +0900

    login,registrationUi

--- app/src/main/java/com/example/perfectholmes/LoginActivity.java
+++ app/src/main/java/com/example/perfectholmes/LoginActivity.java
@@ -2,13 +2,36 @@ package com.example.perfectholmes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText mEmail,mPassword;
+    Button mLoginBtn;
+    TextView mRegisterNowBtn,mIdPwdForget;
+
+    //FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mEmail=findViewById(R.id.emailtxt);
+        mPassword=findViewById(R.id.passwordtxt);
+        mLoginBtn=findViewById(R.id.loginbtn);
+        mRegisterNowBtn=findViewById(R.id.registerNowBtn);
+        mIdPwdForget=findViewById(R.id.idpwfindingbtn);
+
+
+        mRegisterNowBtn.setOnClickListener(
+                view -> startActivity(new Intent(LoginActivity.this,RegistrationActivity.class)));
+
+
+
     }
 }
--- app/src/main/java/com/example/perfectholmes/RegistrationActivity.java
+++ app/src/main/java/com/example/perfectholmes/RegistrationActivity.java
@@ -2,13 +2,38 @@ package com.example.perfectholmes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class RegistrationActivity extends AppCompatActivity {
+    EditText RFullName,REmail,RPassword,Rconpassword;
+    Button RRegisterBtn;
+    TextView RLoginNowBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        RFullName=findViewById(R.id.nameTxt);
+        REmail=findViewById(R.id.regemailTxt);
+        RPassword=findViewById(R.id.RegPwdTxt);
+        Rconpassword=findViewById(R.id.confPwdTxt);
+        RRegisterBtn=findViewById(R.id.regbtn);
+        RLoginNowBtn=findViewById(R.id.loginNowBtn);
+        RLoginNowBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegistrationActivity.this,LoginActivity.class));
+
+            }
+        });
+
+
     }
 }
